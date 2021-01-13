7c91b52315a1cb8d75bb54747e69d9613e581267
Cloud User <Krishanu@demo.jctqairwxy4etnk11ibpekchse.bx.internal.cloudapp.net>
2021-01-13 14:58:46 +0000

    Testing File Update

--- testing.sh
+++ testing.sh
@@ -1,4 +1,4 @@
-status="$(curl -Is http://52.229.188.67:9000/business-2/index.html| head -1)"
+status="$(curl -Is http://40.85.167.219:9000/business-2/index.html| head -1)"
 validate=( $status )
 if [ ${validate[-2]} == "200" ]; then
   echo "OK"
