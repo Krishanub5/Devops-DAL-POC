30918c1c0f696cb9069d358a916bc1d06bce0eb7
Krishanu Bhattacharya <rhel.krishanu@gmail.com>
2021-02-12 08:16:07 +0530

    Update testing.sh

--- testing.sh
+++ testing.sh
@@ -1,4 +1,4 @@
-status="$(curl -Is http://13.92.40.157:9000/business-2/index.html| head -1)"
+status="$(curl -Is http://40.114.111.116:9000/business-2/index.html| head -1)"
 validate=( $status )
 if [ ${validate[-2]} == "200" ]; then
   echo "OK"
